74c53fdfe279c909f45af0eb20470a5debc6b30d
Christopher Goodman <cgoodman@voyages-sncf.com>
2017-06-22 17:07:14 +0200

    changement de chemin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './css/font-awesome.css';
 import './css/bootstrap.min.css';
-import './App.css';
+import './css/App.css';
 import {Game} from './components';
 class App extends React.Component {
     state = {
